4e7b60107813b761bfdb14844fd985056bd1af8c
Kaanah <mateusz.popek@gmail.com>
2015-01-21 22:53:06 +0100

    Works!

--- src/Interface.java
+++ src/Interface.java
@@ -19,6 +19,7 @@ import javax.swing.Action;
 import javax.swing.JFileChooser;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.JTextPane;
 
 public class Interface extends JFrame {
 
@@ -35,6 +36,8 @@ public class Interface extends JFrame {
 	private JLabel lblHeader;
 	private JSpinner spnBottom;
 	private JLabel lblBottom;
+	private JTextPane txtpnCards;
+	private String cardsString = "";
 
 	/**
 	 * Launch the application.
@@ -58,35 +61,42 @@ public class Interface extends JFrame {
 	public Interface() {
 		setTitle("Deckbox parser");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 300);
+		setBounds(100, 100, 779, 776);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[][32.00][][][]", "[][][]"));
-		
-				JButton btnOpenFile = new JButton("Open file...");
-				btnOpenFile.setAction(action);
-				contentPane.add(btnOpenFile, "cell 0 0");
-								
-								spnHeader = new JSpinner();
-								spnHeader.setModel(new SpinnerNumberModel(new Integer(1), null, null, new Integer(1)));
-								contentPane.add(spnHeader, "cell 1 0");
-								
-								lblHeader = new JLabel("Header");
-								contentPane.add(lblHeader, "cell 2 0");
-								
-								spnBottom = new JSpinner();
-								spnBottom.setModel(new SpinnerNumberModel(new Integer(4), null, null, new Integer(1)));
-								contentPane.add(spnBottom, "cell 3 0");
-								
-								lblBottom = new JLabel("Bottom");
-								contentPane.add(lblBottom, "cell 4 0");
-						
-								lblPath = new JLabel("");
-								contentPane.add(lblPath, "cell 0 1");
-				
-						lblFileInfo = new JLabel("");
-						contentPane.add(lblFileInfo, "flowx,cell 0 2");
+		contentPane.setLayout(new MigLayout("", "[grow][32.00][][][]",
+				"[][][][grow]"));
+
+		JButton btnOpenFile = new JButton("Open file...");
+		btnOpenFile.setAction(action);
+		contentPane.add(btnOpenFile, "cell 0 0");
+
+		spnHeader = new JSpinner();
+		spnHeader.setModel(new SpinnerNumberModel(new Integer(1), null, null,
+				new Integer(1)));
+		contentPane.add(spnHeader, "cell 1 0");
+
+		lblHeader = new JLabel("Header");
+		contentPane.add(lblHeader, "cell 2 0");
+
+		spnBottom = new JSpinner();
+		spnBottom.setModel(new SpinnerNumberModel(new Integer(4), null, null,
+				new Integer(1)));
+		contentPane.add(spnBottom, "cell 3 0");
+
+		lblBottom = new JLabel("Bottom");
+		contentPane.add(lblBottom, "cell 4 0");
+
+		lblPath = new JLabel("");
+		contentPane.add(lblPath, "cell 0 1 5 1");
+
+		lblFileInfo = new JLabel("");
+		contentPane.add(lblFileInfo, "flowx,cell 0 2 5 1");
+
+		txtpnCards = new JTextPane();
+		txtpnCards.setText("Copy from here");
+		contentPane.add(txtpnCards, "cell 0 3 5 1,grow");
 
 		fc = new JFileChooser();
 		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -114,14 +124,21 @@ public class Interface extends JFrame {
 			}
 			cards = psr.parse();
 			lblFileInfo.setText("Loaded " + cards.size() + " lines!");
-			for (int i = (Integer)spnHeader.getValue(); i<cards.size() - (Integer)spnBottom.getValue(); i++) {
+			for (int i = (Integer) spnHeader.getValue(); i < cards.size()
+					- (Integer) spnBottom.getValue(); i++) {
 				for (int ii = 0; ii < cards.get(i).length; ii++)
-					if (ii == 0)
+					if (ii == 0){
+						cardsString = cardsString+cards.get(i)[ii] + "x ";
 						System.out.print(cards.get(i)[ii] + "x ");
-					else if (ii == 1)
+					}
+					else if (ii == 1){
+						cardsString = cardsString+cards.get(i)[ii];
 						System.out.print(cards.get(i)[ii]);
+					}
+				cardsString = cardsString+"\n";
 				System.out.println();
 			}
+			txtpnCards.setText(cardsString);
 		}
 	}
 }
