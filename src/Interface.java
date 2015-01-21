83f679bdf27baab739751f8b450045ec6a83dda5
Kaanah <mateusz.popek@gmail.com>
2015-01-21 22:25:36 +0100

    Reads and writes

--- src/Interface.java
+++ src/Interface.java
@@ -17,6 +17,8 @@ import java.util.List;
 
 import javax.swing.Action;
 import javax.swing.JFileChooser;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 public class Interface extends JFrame {
 
@@ -29,6 +31,10 @@ public class Interface extends JFrame {
 	private JLabel lblPath;
 	private JLabel lblFileInfo;
 	private List<String[]> cards;
+	private JSpinner spnHeader;
+	private JLabel lblHeader;
+	private JSpinner spnBottom;
+	private JLabel lblBottom;
 
 	/**
 	 * Launch the application.
@@ -52,22 +58,36 @@ public class Interface extends JFrame {
 	public Interface() {
 		setTitle("Deckbox parser");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[][]", "[][]"));
+		contentPane.setLayout(new MigLayout("", "[][32.00][][][]", "[][][]"));
 		
-		JButton btnOpenFile = new JButton("Open file...");
-		btnOpenFile.setAction(action);
-		contentPane.add(btnOpenFile, "cell 0 0");
-		
-		lblPath = new JLabel("");
-		contentPane.add(lblPath, "cell 1 0");
-		
-		lblFileInfo = new JLabel("File info:");
-		contentPane.add(lblFileInfo, "cell 0 1");
-		
+				JButton btnOpenFile = new JButton("Open file...");
+				btnOpenFile.setAction(action);
+				contentPane.add(btnOpenFile, "cell 0 0");
+								
+								spnHeader = new JSpinner();
+								spnHeader.setModel(new SpinnerNumberModel(new Integer(1), null, null, new Integer(1)));
+								contentPane.add(spnHeader, "cell 1 0");
+								
+								lblHeader = new JLabel("Header");
+								contentPane.add(lblHeader, "cell 2 0");
+								
+								spnBottom = new JSpinner();
+								spnBottom.setModel(new SpinnerNumberModel(new Integer(4), null, null, new Integer(1)));
+								contentPane.add(spnBottom, "cell 3 0");
+								
+								lblBottom = new JLabel("Bottom");
+								contentPane.add(lblBottom, "cell 4 0");
+						
+								lblPath = new JLabel("");
+								contentPane.add(lblPath, "cell 0 1");
+				
+						lblFileInfo = new JLabel("");
+						contentPane.add(lblFileInfo, "flowx,cell 0 2");
+
 		fc = new JFileChooser();
 		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
 	}
@@ -75,15 +95,17 @@ public class Interface extends JFrame {
 	private class SwingAction extends AbstractAction {
 
 		private static final long serialVersionUID = 1L;
+
 		public SwingAction() {
 			putValue(NAME, "Open file...");
 			putValue(SHORT_DESCRIPTION, "Opens .csv file for parsing");
 		}
+
 		public void actionPerformed(ActionEvent e) {
 			int returnVal = fc.showOpenDialog(Interface.this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				fileOpen = fc.getSelectedFile();
-				lblPath.setText("File: "+fileOpen.getPath());
+				lblPath.setText("File: " + fileOpen.getPath());
 				try {
 					psr = new Parser(fileOpen);
 				} catch (InvalidFileFormatException e1) {
@@ -91,10 +113,14 @@ public class Interface extends JFrame {
 				}
 			}
 			cards = psr.parse();
-			for(String[] card:cards){
-				for(String part:card){
-					System.out.println(part);
-				}
+			lblFileInfo.setText("Loaded " + cards.size() + " lines!");
+			for (int i = (Integer)spnHeader.getValue(); i<cards.size() - (Integer)spnBottom.getValue(); i++) {
+				for (int ii = 0; ii < cards.get(i).length; ii++)
+					if (ii == 0)
+						System.out.print(cards.get(i)[ii] + "x ");
+					else if (ii == 1)
+						System.out.print(cards.get(i)[ii]);
+				System.out.println();
 			}
 		}
 	}
