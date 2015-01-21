3cc2798d38e97f4ea930a1b57e219a6cb75ee7ae
Kaanah <mateusz.popek@gmail.com>
2015-01-21 20:31:13 +0100

    Checks if file is in proper format

---
+++ src/InvalidFileFormatException.java
@@ -0,0 +1,4 @@
+
+public class InvalidFileFormatException extends Exception {
+
+}
--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,23 +7,41 @@ import java.util.List;
 import com.opencsv.CSVReader;
 
 public class Parser {
-	private String path;
+	private File file;
 
-	public Parser(String path) {
-		this.path = path;
+	public Parser(File file) throws InvalidFileFormatException{
+		this.file = file;
+		if(!getExtension(file).equals("csv"))
+			throw new InvalidFileFormatException();
 	}
 
-	public void parse() {
+	public List parse() {
 		CSVReader reader;
+		List<String[]> entries = null;
 		try {
-			reader = new CSVReader(new FileReader(path));
+			reader = new CSVReader(new FileReader(file));
 			try {
-				List<String[]> entries = reader.readAll();
+				entries = reader.readAll();
+				reader.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		return entries;
+	}
+	
+	public String getExtension(File f){
+		String extension = "";
+		String fileName = f.getName();
+		int i = fileName.lastIndexOf('.');
+		int p = Math.max(fileName.lastIndexOf('/'), fileName.lastIndexOf('\\'));
+
+		if (i > p) {
+		    extension = fileName.substring(i+1);
+		}
+		
+		return extension;
 	}
 }
